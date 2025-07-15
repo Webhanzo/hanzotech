34f9fad55e332fc9f4249de69adb568398203c8a
Hanzo Web <webhanzo754@gmail.com>
2025-07-15 06:57:08 +0000

    [09:52:15.749] Running build in Washington, D.C., USA (East) – iad1 [09:

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -4,14 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDAKu4z1mYcd3uHMYkVaOuPdoKatXtV9EE",
+  authDomain: "hanzotech.firebaseapp.com",
+  databaseURL: "https://hanzotech-default-rtdb.firebaseio.com",
+  projectId: "hanzotech",
+  storageBucket: "hanzotech.appspot.com",
+  messagingSenderId: "109986788087",
+  appId: "1:109986788087:web:aa263c2a206fb6faaed9f2"
 };
 
 // Initialize Firebase
