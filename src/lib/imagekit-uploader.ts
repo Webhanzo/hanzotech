8e077acfcd916ad125fd4249c163e9fe6a14f24d
Hanzo Web <webhanzo754@gmail.com>
2025-11-03 20:54:00 +0000

    قمت برفع صورة، اظهر خطأ يرجى التحقق من اعدادات image kit ما هي اسباب الخ

--- src/lib/imagekit-uploader.ts
+++ src/lib/imagekit-uploader.ts
@@ -7,7 +7,7 @@ const getImageKitInstance = () => {
     if (!ikInstance) {
         ikInstance = new ImageKit({
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!, 
+            publicKey: process.env.IMAGEKIT_PUBLIC_KEY!, 
             authenticationEndpoint: `${window.location.origin}/api/imagekit/auth`,
         });
     }
